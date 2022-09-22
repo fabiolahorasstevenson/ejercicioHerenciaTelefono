3263edc44a940f476e6c2910dd2b757c651e5a49
Fabiola Horas <fabiolahoras@gmail.com>
2022-09-21 22:00:03 -0300

    first commit

---
+++ telefono.ts
@@ -0,0 +1,56 @@
+class telefono{
+    private estaPrendido: boolean;
+    private bateriaActual: boolean;
+
+    constructor(pEstaPrendido: boolean, pBateriaActual: boolean){
+        this.bateriaActual = pBateriaActual;
+        this.estaPrendido = pEstaPrendido;
+
+    }
+
+    public mandarMensaje(pNumeroTelefono: number):void{
+        console.log("Mandando mensaje a"+pNumeroTelefono);
+
+    }
+
+    public hacerLlamada(pNumeroTelefono: number):void{
+        console.log("Llamando a "+pNumeroTelefono);
+    }
+
+    public prenderApagar(){
+        if (this.estaPrendido== true){
+            this.estaPrendido = false;
+        }
+        else{
+            this.estaPrendido = true;
+        }
+    }
+
+}
+
+class telefonoConCamara extends telefono{
+
+    constructor(pEstaPrendido: boolean, pBateriaActual: boolean){
+        super (pEstaPrendido, pBateriaActual);
+    }
+
+    public sacarFotos(){
+        console.log("Se ha sacado la foto deseada ");
+
+    }
+}
+
+class telefonoConRadio extends telefono{
+
+    private frencuenciaActual : number;
+    
+    constructor(pEstaPrendido: boolean, pBateriaActual: boolean, pFrecuenciaActual: number){
+        super (pEstaPrendido, pBateriaActual);
+        this.frencuenciaActual = pFrecuenciaActual;
+    }
+    public verFrecuenciaActual(){
+        return this.frencuenciaActual;
+    }
+}
+
+let telefono1 = new telefono(true,true);
